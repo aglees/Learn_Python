9496717cc93d35103a6ddc38cf5799e4e377324e
Andrew Gleeson <GleesA10@interoute.com>
2017-01-04 14:01:21 +0000

    Update to test array

--- PythonInterviewProblems/Largest_Continual_Sum.py
+++ PythonInterviewProblems/Largest_Continual_Sum.py
@@ -48,13 +48,11 @@ def large_cont_sum_answer(arr):
     
     max_sum = current_sum = arr[0]
     
-    for num in arr[1:]:
-        
+    for num in arr[1:]:    
         current_sum = max(current_sum+num, num)
-        
         max_sum = max(current_sum, max_sum)
-    
     return max_sum
 
 lst = [-1,1]
-print (large_cont_sum(lst))
+lst = [1,2,-1,3,4,10,10,-10,-1]
+print (large_cont_sum_answer(lst))
