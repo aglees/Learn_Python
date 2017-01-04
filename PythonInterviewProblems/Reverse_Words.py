f13a8746f16b6816fd32c4bc46b01560308955c5
Andrew Gleeson <GleesA10@interoute.com>
2017-01-04 14:25:10 +0000

    Added one liners to Reverse_Words

--- PythonInterviewProblems/Reverse_Words.py
+++ PythonInterviewProblems/Reverse_Words.py
@@ -10,4 +10,12 @@ def rev_word(s):
     
     return mystring
 
+def rev_word2(s):
+    return " ".join(reversed(s.split()))
+
+def rev_word3(s):
+    return " ".join(s.split()[::-1])
+
 print (rev_word('Hi John,   are you ready to go?'))
+print (rev_word2('Hi John,   are you ready to go?'))
+print (rev_word3('Hi John,   are you ready to go?'))
