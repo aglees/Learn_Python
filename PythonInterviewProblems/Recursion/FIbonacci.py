7374dc284a0a0bb1ad3bc3b7954dab184344ee18
Andrew Gleeson <andrew.gleeson@gmail.com>
2017-01-07 15:49:41 +0000

    updated fivs

--- PythonInterviewProblems/Recursion/FIbonacci.py
+++ PythonInterviewProblems/Recursion/FIbonacci.py
@@ -14,11 +14,38 @@ def fib_dynamic(n, results=None):
     while n not in results:
         fib_1 = fib_dynamic(n-1, results)
         fib_2 = fib_dynamic(n-2, results)
-        fib =  fib_1 + fib_2
+        fib = fib_1 + fib_2
         results[n] = fib
         
     return results[n]
 
+def fib_dynamic2(n, results=None):
+
+    if not results:
+        results = {0:0, 1:1}
+
+    while n not in results:
+        fib = fib_dynamic2(n-1, results) + fib_dynamic2(n-2, results)
+        results[n] = fib
+
+    return results[n]
+
+def fib_iter(n):
+    
+    series = [0,1]
+
+    if n <= 1:
+        return n
+
+    for i in range(2,n+1):
+
+        sum = series[i-1] + series[i-2]
+
+        series.append(sum)
+
+    return sum
+
 # print(fib_recursion(10))
-print(fib_dynamic(100))
-# 0 1 1 2 3 5
+# print(fib_dynamic(30))
+# print(fib_dynamic2(30))
+print(fib_iter(10))
